c9e14f161075374e14c4816b3c4650822018cdb7
Jarred Parr <jparr721@gmail.com>
2022-02-06 10:18:40 -0700

    parallel grid op, moving to p2g, g2p

--- src/nclr.cpp
+++ src/nclr.cpp
@@ -174,46 +174,6 @@ auto nclr_grid_op(const int res, const double dx, const double dt, const double 
     }
 }
 
-/* auto nclr_mpm(const double inv_dx, const double hardening, const double mu_0, const double lambda_0, const double mass, */
-/*               const double dx, const double dt, const double volume, const unsigned int res, const double gravity, */
-/*               const std::size_t timesteps, const Eigen::MatrixX3d &x) -> std::vector<Eigen::MatrixXd> { */
-/*     std::vector<std::vector<Eigen::Vector3d>> out; */
-/*     out.reserve(timesteps); */
-
-/*     std::vector<Eigen::Vector3d> positions; */
-/*     for (int row = 0; row < x.rows(); ++row) { positions.push_back(x.row(row)); } */
-/*     auto v = std::vector<Eigen::Vector3d>(x.rows(), Eigen::Vector3d::Zero()); */
-/*     auto F = std::vector<Eigen::Matrix3d>(x.rows(), diagonal(1.0)); */
-/*     auto C = std::vector<Eigen::Matrix3d>(x.rows(), Eigen::Matrix3d::Zero()); */
-/*     auto Jp = std::vector<double>(x.rows(), 1); */
-
-/*     for (int ii : tqdm::range(timesteps)) { */
-/*         auto grid_velocity = std::vector<Eigen::Vector3d>(res * res * res, Eigen::Vector3d::Zero()); */
-/*         auto grid_mass = std::vector<double>(res * res * res, 0); */
-
-/*         nclr_p2g(res, inv_dx, hardening, mu_0, lambda_0, mass, dx, dt, volume, grid_velocity, grid_mass, positions, v, */
-/*                  F, C, Jp, MaterialModel::kNeoHookean); */
-/*         nclr_grid_op(res, dx, dt, gravity, grid_mass, grid_velocity); */
-/*         nclr_g2p(res, inv_dx, dt, grid_velocity, positions, v, F, C, Jp, MaterialModel::kNeoHookean); */
-/*         out.push_back(positions); */
-/*     } */
-
-/*     std::vector<Eigen::MatrixXd> all_positions; */
-
-/*     for (const auto &pos : out) { all_positions.push_back(vec_to_mat(pos)); } */
-
-/*     return all_positions; */
-/* } */
-
-/* PYBIND11_MODULE(nuclear_mpm, m) { */
-/*     m.doc() = "Fast offline MPM solver"; */
-
-/*     m.def("nclr_mpm", &nclr_mpm); */
-/*     m.def("nclr_fixed_corotated_stress", &nclr_fixed_corotated_stress); */
-/*     m.def("nclr_polar", &nclr_polar); */
-/* } */
-
-
 // Window
 constexpr int window_size = 1200;
 
@@ -243,9 +203,12 @@ std::vector<Particle> particles;
 int nn = n + 1;
 
 auto nclr_grid_op(const float gravity, std::vector<Cell> &grid) -> void {
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
-            const auto index = (i * nn) + j;
+    // boundary thickness
+    constexpr int boundary = 3;
+#pragma omp parallel for collpase(2)
+    for (int ii = 0; ii <= n; ii++) {
+        for (int jj = 0; jj <= n; jj++) {
+            const auto index = (ii * nn) + jj;
             auto &g = grid[index];
             // No need for epsilon here
             if (g.mass > 0.0) {
@@ -253,22 +216,33 @@ auto nclr_grid_op(const float gravity, std::vector<Cell> &grid) -> void {
                 g.velocity /= g.mass;
                 // Gravity
                 g.velocity(1) += dt * gravity;
-
-                // boundary thickness
-                const real boundary = 0.05;
-
-                // Node coordinates
-                real x = real(i) / n;
-                real y = real(j) / n;
-
-                // Sticky boundary
-                if (x < boundary || x > 1 - boundary || y > 1 - boundary || y < boundary) {
-                    g.velocity = constvec(0);
-                    g.mass = 0.0;
-                }
-                // Separate boundary (needs friction)
-                /* if (y < boundary) { g.velocity[1] = std::max(0.0f, g.velocity[1]); } */
             }
+
+            // Node coordinates
+            /* real x = real(i) / n; */
+            /* real y = real(j) / n; */
+
+            // Boundary conditions
+            /* if (ii < boundary && grid.at(index).velocity(0) < 0) { grid.at(index).velocity(0) = 0; } */
+            /* if (ii >= nn - boundary && grid.at(index).velocity(0) > 0) { grid.at(index).velocity(0) = 0; } */
+            /* if (jj < boundary && grid.at(index).velocity(1) < 0) { grid.at(index).velocity(1) = 0; } */
+            /* if (jj >= nn - boundary && grid.at(index).velocity(1) > 0) { grid.at(index).velocity(1) = 0; } */
+
+            if (ii < boundary && grid.at(index).velocity(0) < 0 ||
+                ii >= nn - boundary && grid.at(index).velocity(0) > 0 ||
+                jj < boundary && grid.at(index).velocity(1) < 0 ||
+                jj >= nn - boundary && grid.at(index).velocity(1) > 0) {
+                g.velocity = constvec(0);
+                g.mass = 0.0;
+            }
+
+            // Sticky boundary
+            /* if (x < boundary || x > 1 - boundary || y > 1 - boundary || y < boundary) { */
+            /*     g.velocity = constvec(0); */
+            /*     g.mass = 0.0; */
+            /* } */
+            // Separate boundary (needs friction)
+            /* if (y < boundary) { g.velocity[1] = std::max(0.0f, g.velocity[1]); } */
         }
     }
 }
