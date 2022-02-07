843b631328e2cf5452ffe0690ed8ecc003ff0079
Jarred Parr <jparr721@gmail.com>
2022-02-07 16:35:25 -0700

    fix example code

--- src/nclr.cpp
+++ src/nclr.cpp
@@ -4,25 +4,23 @@
 #include <cstdint>
 #include <memory>
 
-namespace nclr {
-    // How many dimensions to run the sim in (2 or 3)
-    constexpr int kDimension = 2;
+// How many dimensions to run the sim in (2 or 3)
+constexpr int kDimension = 2;
 
-    // The resolution of the cube meshes.
-    constexpr int kResolution = 50;
+// The resolution of the cube meshes.
+constexpr int kResolution = 50;
 
-    // Window
-    constexpr int kWindowSize = 800;
+// Window
+constexpr int kWindowSize = 800;
 
-    // Timestep size
-    constexpr real dt = 1e-4f;
+// Timestep size
+constexpr nclr::real dt = 1e-4f;
 
-    // Frame draw interval
-    constexpr real frame_dt = 1e-3f;
+// Frame draw interval
+constexpr nclr::real frame_dt = 1e-3f;
 
-    // The color to paint the points
-    constexpr int color = 0xED553B;
-}// namespace nclr
+// The color to paint the points
+constexpr int color = 0xED553B;
 
 int main() {
     using namespace nclr;
